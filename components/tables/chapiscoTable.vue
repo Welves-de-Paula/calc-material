6f767b1cb7cb8218cdb7a8af78f023d2a6612871
welves2001 <welvesdepaula@gmail.com>
2020-08-21 17:00:28 -0300

    dev

--- components/tables/chapiscoTable.vue
+++ components/tables/chapiscoTable.vue
@@ -1,5 +1,5 @@
 <template>
-<v-card class="mx-auto" max-width="400" tile>
+<v-card class="mx-auto" max-width="500" tile>
 
     <v-list-item>
         <v-list-item-content>
@@ -7,39 +7,59 @@
             </v-list-item-title>
         </v-list-item-content>
     </v-list-item>
-
     <v-list-item>
         <v-list-item-content>
             <v-list-item-title>Finalidade</v-list-item-title>
         </v-list-item-content>
-
     </v-list-item>
+
+    <v-list-item-group v-model="settings" multiple>
+        <v-list-item>
+            <template v-slot:default="{ active }">
+                <v-list-item-action>
+                    <v-checkbox :input-value="active" color="primary"></v-checkbox>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>Teto</v-list-item-title>
+                </v-list-item-content>
+            </template>
+        </v-list-item>
+
+        <v-list-item>
+            <template v-slot:default="{ active }">
+                <v-list-item-action>
+                    <v-checkbox :input-value="active" color="primary"></v-checkbox>
+                </v-list-item-action>
+
+                <v-list-item-content>
+                    <v-list-item-title>Superfície de concreto</v-list-item-title>
+                </v-list-item-content>
+            </template>
+        </v-list-item>
+
+        <v-list-item>
+            <template v-slot:default="{ active }">
+                <v-list-item-action>
+                    <v-checkbox :input-value="active" color="primary"></v-checkbox>
+                </v-list-item-action>
+
+                <v-list-item-content>
+                    <v-list-item-title>Parede</v-list-item-title>
+                </v-list-item-content>
+            </template>
+        </v-list-item>
+    </v-list-item-group>
 
     <v-list-item>
         <v-list-item-content>
-            <v-list-item-title>Teto</v-list-item-title>
+            <v-list-item-title>Espesura</v-list-item-title>
         </v-list-item-content>
-
-    </v-list-item>
-
-    <v-list-item>
-        <v-list-item-content>
-            <v-list-item-title>Superfície de concreto</v-list-item-title>
-        </v-list-item-content>
-
-    </v-list-item>
-    <v-list-item>
-        <v-list-item-content>
-            <v-list-item-title>Parede</v-list-item-title>
-        </v-list-item-content>
-    </v-list-item>
-
-    <span>Espesura</span>
     <v-chip-group>
-        <v-chip filter class="ma-2">Chapisco Grosso 5</v-chip>
-        <v-chip filter class="ma-2">Chapisco Fino 3</v-chip>
-        <v-chip filter class="ma-2"> Personalizado</v-chip>
+        <v-chip label  filter >Chapisco Grosso </v-chip>
+        <v-chip label filter >Chapisco Fino</v-chip>
+        <v-chip label filter > Personalizado</v-chip>
     </v-chip-group>
+    </v-list-item>
 
     <v-list-item>
         <v-list-item-content>
